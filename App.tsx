541376d214ec89e6098bf53a5ca9689d17415bf7
Jean-Marie Dalmasso <jean-marie.dalmasso@hp.com>
2021-07-28 09:51:40 -0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the Redux TypeScript template
+ * https://github.com/rahsheen/react-native-template-redux-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  View,
+} from 'react-native';
+import {Counter} from './src/features/counter/Counter';
+
+const App = () => {
+  console.log('La la la la')
+  return (
+    <View style={{marginTop: 55}}>
+      <Counter />
+    </View>
+  );
+};
+
+export default App;
